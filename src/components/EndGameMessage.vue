c8d6e9bdb74020295ac5d5cb2d514d7651086a0a
rajdun <xdrpl445@gmail.com>
2024-11-04 21:21:46 +0100

    end game message animations

--- src/components/EndGameMessage.vue
+++ src/components/EndGameMessage.vue
@@ -5,7 +5,8 @@ import { computed, ref } from 'vue'
 
 const game = useGame();
 
-let displayMessage = ref(true);
+const displayMessage = ref(true);
+const animateWindow = ref(false);
 
 const gameTime = computed(() => {
   const duration = game.endGame - game.startGame;
@@ -21,11 +22,28 @@ const gameTime = computed(() => {
     return `${seconds}s ${miliseconds}ms`;
   }
 });
+
+function showWindow() {
+  displayMessage.value = !displayMessage.value;
+}
+
+function hideWindow() {
+  animateWindow.value = true;
+  setTimeout(() => {
+    displayMessage.value = !displayMessage.value;
+    animateWindow.value = false;
+  }, 500);
+}
 </script>
 
 <template>
-  <div v-if="displayMessage" class="container" :class="game.state === 'gameWon' ? 'greenBorder' : 'redBorder'">
-    <button @click="displayMessage=false" class="button close-button" :class="game.state === 'gameWon' ? 'bg-success' : 'bg-danger'">X</button>
+  <button @click="showWindow" class="showWindowButton button clickable" v-if="!displayMessage">
+    <img src="/arrowUp.svg" alt="Arrow Up" />
+  </button>
+  <div v-if="displayMessage" class="container" :class="[{'greenBorder': game.state === 'gameWon', 'redBorder': game.state !== 'gameWon'}, {'hideWindow': animateWindow}]">
+    <button @click="hideWindow" class="button close-button" :class="game.state === 'gameWon' ? 'bg-success' : 'bg-danger'">
+      <img src="/close.svg" alt="Close" />
+    </button>
     <div>
       <h1 v-if="game.state === 'gameOver'" class="color-danger">Game Over</h1>
       <h1 v-else-if="game.state === 'gameWon'" class="color-success">You Won!</h1>
@@ -38,30 +56,51 @@ const gameTime = computed(() => {
 <style scoped>
 h1 {
   font-size: 8rem;
+
+  @media (max-width: 768px) {
+    font-size: 3rem;
+  }
 }
 h2 {
   font-size: 5rem;
+
+  @media (max-width: 768px) {
+    font-size: 2rem;
+  }
+}
+
+.showWindowButton {
+  position: fixed;
+  bottom: 0.5rem;
+  right: calc(50vw - 2rem);
+  padding: 0.5rem;
+  font-size: 2rem;
+  border-radius: 2rem 2rem 0 0;
+  cursor: pointer;
+  z-index: 100;
+  animation: jumpFromBottom 0.5s ease-in-out;
+  animation-fill-mode: forwards;
+}
+
+.hideWindow {
+  animation: jumpToBottom 0.5s ease-in-out !important;
+  animation-fill-mode: forwards;
 }
 
 .close-button {
   position: absolute;
-  top: -1px;
-  right: -2px;
+  top: 0.5rem;
+  right: 0.5rem;
+  padding: 0 1rem;
   font-size: 2rem;
-  padding: 1rem;
-  color: var(--color-text);
-  border: none;
-  border-radius: 0 2rem 0 1rem;
+  border-radius: 2rem 2rem;
   cursor: pointer;
+
+  @media (max-width: 768px) {
+    padding: 0.5rem;
+  }
 }
 
-.bg-success {
-  background-color: var(--color-success);
-}
-
-.bg-danger {
-  background-color: var(--color-danger);
-}
 
 .greenBorder {
   border: 0.25rem solid var(--color-success);
@@ -75,6 +114,10 @@ h2 {
   margin: 1rem;
   padding: 2rem;
   font-size: 4rem;
+
+  @media (max-width: 768px) {
+    font-size: 1.25rem;
+  }
 }
 .container {
   position: fixed;
@@ -92,5 +135,14 @@ h2 {
   animation: jumpFromBottom 0.5s ease-in-out;
   animation-fill-mode: forwards;
   border-radius: 2rem;
+  transition: transform 0.5s ease-in-out;
+
+  @media (max-width: 768px) {
+    top: 66vh;
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    border-radius: 2rem 2rem 0 0;
+  }
 }
 </style>
