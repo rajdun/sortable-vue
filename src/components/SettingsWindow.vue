9318e08e001d86a0a160f4bfe251f8bdd1dc10da
rajdun <xdrpl445@gmail.com>
2024-11-07 20:34:11 +0100

    difficulty input style

--- src/components/SettingsWindow.vue
+++ src/components/SettingsWindow.vue
@@ -1,12 +1,20 @@
 <script setup>
 
 import { useGame } from '@/stores/game.js'
+import { onMounted, ref } from 'vue'
 
 const game = useGame();
+const currentDifficulty = ref('medium');
 
 function setDifficulty(difficulty) {
   game.setDifficulty(difficulty);
 }
+
+onMounted(() => {
+  currentDifficulty.value = game.getDifficulty();
+
+  document.querySelector(`input[value="${currentDifficulty.value}"]`).checked = true;
+})
 
 </script>
 
@@ -15,17 +23,19 @@ function setDifficulty(difficulty) {
     <h1>Settings</h1>
     <hr>
     <div class="settingsGroup">
-      <h2>Difficulty</h2>
-      <div class="difficulty">
-        <button @click="setDifficulty('easy')">Easy</button>
-        <button @click="setDifficulty('medium')">Medium</button>
-        <button @click="setDifficulty('hard')">Hard</button>
+      <div class="difficulty-input-group">
+        <span class="optionTitle">Difficulty</span>
+        <span class="difficulty">
+          <input @click="setDifficulty('easy')" type="radio" name="difficulty" value="easy" id="">
+          <input @click="setDifficulty('medium')" type="radio" name="difficulty" value="medium" id="">
+          <input @click="setDifficulty('hard')" type="radio" name="difficulty" value="hard" id="">
+        </span>
       </div>
     </div>
   </div>
 </template>
 
-<style scoped>
+<style scoped lang="scss">
  .settingsWindow{
     position: fixed;
     top: 50vh;
@@ -39,10 +49,68 @@ function setDifficulty(difficulty) {
     z-index: 200;
     padding: 1.5rem;
     border-radius: 2rem;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.5);
+    text-align: left;
+
+   @media (max-width: 768px) {
+     left: 0;
+     right: 0;
+     bottom: 0;
+     border-radius: 0;
+     justify-content: normal;
+   }
  }
+
+ .difficulty-input-group{
+   display: flex;
+   gap: 1rem;
+   align-items: center;
+ }
+
+ h1{
+    font-size: 2.5rem;
+    font-weight: bold;
+ }
+
+.optionTitle{
+  font-size: 1.5rem;
+}
 
  hr {
    width: 100%;
    margin: 1rem 0;
+ }
+
+ .difficulty {
+   display: flex;
+   align-items: normal;
+ }
+
+ .difficulty input[type="radio"] {
+   appearance: none;
+   -webkit-appearance: none;
+   width: 1.51rem;
+   height: 1rem;
+   margin: 0 0.15rem;
+   border: 0.2rem solid white;
+   background-color: var(--color-background-soft);
+   position: relative;
+ }
+
+ .difficulty input[type="radio"]:checked {
+   background-color: white; /* Change this to your desired color when selected */
+ }
+
+ /* Hide the connector after the last radio button */
+ .difficulty input[type="radio"]:last-child:after {
+   display: none;
+ }
+
+ .difficulty input[type="radio"]:last-child {
+   border-radius: 0 1rem 1rem 0;
+ }
+
+ .difficulty input[type="radio"]:first-child {
+   border-radius: 1rem 0 0 1rem;
  }
 </style>
